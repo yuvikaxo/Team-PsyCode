49a4b00bdc79738f72a050a75c041dfad4f9b1bb
Arshil Mac <mohdarshilmbd1@gmail.com>
2025-04-01 22:06:16 +0530

    frontend started

---
+++ Frontend/zendrive/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="bg-white">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
